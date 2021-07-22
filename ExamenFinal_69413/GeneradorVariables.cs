fe561c5ebb9725e813cab337cbba7eb60c0ed7b8
Marcelo Brizuela <marceloestebanbrizuelavega@gmail.com>
2021-07-22 03:32:53 -0300

    Primeros planteos

---
+++ ExamenFinal_69413/GeneradorVariables.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenFinal_69413
+{
+    public class GeneradorVariables
+    {
+        //Para determinar que formula vamos a usar en la normal
+        // true=primer funcion
+        // false=segunda funcion
+        private static bool normalUsado = true;
+
+
+        //LOS METODOS DEL GENERADOR SOLO DEVUELVEN UN VALOR SALVO EL NORMAL QUE DEVUELVE UN ARRAY DE DOS VALORES
+
+
+        //Instanciacion del generador de  valores aleatorios
+        Random rnd = new Random();
+
+        //Distribucion Uniforme
+        public double Uniforme(int limiteInferior, int limiteSuperior, double rnd)
+        {
+            int a = limiteInferior;
+            int b = limiteSuperior;
+
+            //Declaración del numero aleatorio
+            double valores;
+            //Se suma A a un numero aleatorio que se encuentra entre
+            //el Delta que se encuentre entre A y B      
+            valores = a + rnd * (b - a);
+
+            return valores;
+        }
+
+        //Distribucion Exponencial (No se utiliza)
+        public double Exponencial(double lambda)
+        {
+            double l = lambda;
+
+            //Declaracion del Array
+            double valores;
+            //Recorrido del array
+
+            valores = (-1 / l) * (Math.Log(1 - rnd.NextDouble()));
+
+            return valores;
+        }
+
+        //Distribucion Poisson (No se utiliza)
+        public double[] Poisson(double lambda, int cantidadValores)
+        {
+            double l = lambda;
+            int n = cantidadValores;
+            //Declaracion de constantes
+            //Declaracion del Array
+            double[] valores = new double[n];
+            double a = Math.Pow(Math.E, -l);
+            for (int i = 0; i < n; i++)
+            {
+                double p = 1;
+                double x = -1;
+                while (p >= a)
+                {
+                    double u = rnd.NextDouble();
+                    p = p * u;
+                    x = x + 1;
+                }
+                valores[i] = x;
+
+            }
+            return valores;
+        }
+
+
+        //Distribucion Normal
+        public double Normal(double random1, double random2, double media, double varianza)
+        {
+            double u = media;
+            double v = varianza;
+            double valor;
+
+            //Declaracion del Array
+            //double[] valores = new double[2];
+            //Declaracion del valor PHI
+            double PI = 3.1415926535897931;
+
+            //Declaracion de dos numeros aleatorios RND1 y RND2
+            //double rnd1 = rnd.NextDouble();
+            //double rnd2 = rnd.NextDouble();
+            if (normalUsado)
+            {
+                //Calculo del primer numero
+                valor = (Math.Sqrt(-2 * Math.Log(random1)) * Math.Cos(2 * PI * random2)) * Math.Sqrt(v) + u;
+            }
+            else
+            {
+                //Calculo del segundo numero
+                valor = (Math.Sqrt(-2 * Math.Log(random1)) * Math.Sin(2 * PI * random2)) * Math.Sqrt(v) + u;
+            }
+
+            normalUsado = !normalUsado;
+            return valor;
+        }
+    }
+}
---
+++ ExamenFinal_69413/Herramientas.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExamenFinal_69413
+{
+    class Herramientas
+    {
+        public static double TruncadoMarcelo(double valor, int cantATruncar)
+        {
+            int factor = Convert.ToInt32(Math.Pow(10, cantATruncar));
+            return (Math.Truncate(valor * factor)) / factor;
+        }
+
+        public static void setearTipoEstado(DataGridView grilla)
+        {
+
+            for (int i = 0; i < grilla.Rows.Count; i++)
+            {
+                switch (grilla.Rows[i].Cells[13].Value)
+                {
+                    case "0":
+                        grilla.Rows[i].Cells[13].Value = "Libre";
+                        break;
+                    case "1":
+                        grilla.Rows[i].Cells[13].Value = "Ocupado";
+                        break;
+                    case "3":
+                        grilla.Rows[i].Cells[13].Value = "Purgando";
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            for (int i = 0; i < grilla.Rows.Count; i++)
+            {
+                switch (grilla.Rows[i].Cells[24].Value)
+                {
+                    case "0":
+                        grilla.Rows[i].Cells[24].Value = "Libre";
+                        break;
+                    case "1":
+                        grilla.Rows[i].Cells[24].Value = "Ocupado";
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public static void setearEvento(DataGridView grilla)
+        {
+            for (int i = 0; i < grilla.Rows.Count; i++)
+            {
+                switch (grilla.Rows[i].Cells[1].Value)
+                {
+                    case "0":
+                        grilla.Rows[i].Cells[1].Value = "Inicializacion";
+                        break;
+                    case "4":
+                        grilla.Rows[i].Cells[1].Value = "Llegada_Armazon";
+                        break;
+                    case "8":
+                        grilla.Rows[i].Cells[1].Value = "Llegada_Motor";
+                        break;
+                    case "20":
+                        grilla.Rows[i].Cells[1].Value = "Llegada_Ruedas";
+                        break;
+                    case "11":
+                        grilla.Rows[i].Cells[1].Value = "Fin_Ensamblaje";
+                        break;
+                    case "14":
+                        grilla.Rows[i].Cells[1].Value = "Inestabilidad";
+                        break;
+                    case "15":
+                        grilla.Rows[i].Cells[1].Value = "Fin_Purga";
+                        break;
+                    case "23":
+                        grilla.Rows[i].Cells[1].Value = "Fin_Area_Rueda";
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public static void matrizAGrid(double[,] matriz, System.Windows.Forms.DataGridView dataGridView, int ordenTruncado)
+        {
+            if (dataGridView.Rows.Count == 0)
+            {
+                for (int i = 0; i < matriz.GetLength(0); i++)
+                {
+                    dataGridView.Rows.Add();
+                    for (int c = 0; c < matriz.GetLength(1); c++)
+                    {
+                        dataGridView.Rows[i].Cells[c].Value = TruncadoMarcelo(matriz[i, c], ordenTruncado).ToString();
+                    }
+                }
+            }
+            else
+            {
+
+                for (int j = 0; j < matriz.GetLength(0); j++)
+                {
+                    dataGridView.Rows.Add();
+
+                    for (int i = 0; i < matriz.GetLength(1); i++)
+                    {
+                        dataGridView.Rows[dataGridView.Rows.Count - 1].Cells[i].Value = TruncadoMarcelo(matriz[j, i], ordenTruncado).ToString();
+                    }
+                }
+
+            }
+
+        }
+
+        public static void truncarDataGrid(DataGridView dataGridView, int ordenTruncado)
+        {
+            for (int i = 0; i < dataGridView.Rows.Count; i++)
+            {
+                for (int j = 0; j < dataGridView.Columns.Count; j++)
+                {
+                    if (dataGridView.Rows[i].Cells[j].Value is Double)
+                    {
+                        dataGridView.Rows[i].Cells[j].Value = TruncadoMarcelo(Convert.ToDouble(dataGridView.Rows[i].Cells[j].Value), 4);
+                    }
+
+                }
+            }
+        }
+
+        public static void matrizAGrid2(double[,] vs, DataGridView data, int ordenTruncado)
+        {
+
+            if (data.Rows.Count == 0)
+            {
+                data.Rows.Add();
+                for (int i = 0; i < vs.GetLength(1); i++)
+                {
+                    data.Rows[data.Rows.Count - 1].Cells[i].Value = TruncadoMarcelo(vs[0, i], ordenTruncado).ToString();
+                }
+            }
+            else
+            {
+                data.Rows.Add();
+                for (int i = 0; i < vs.GetLength(1); i++)
+                {
+                    data.Rows[data.Rows.Count - 1].Cells[i].Value = TruncadoMarcelo(vs[1, i], ordenTruncado).ToString();
+                }
+            }
+        }
+
+    }
+}
+
